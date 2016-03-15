4be020f2ce3354167d7ceaa43aeb7a34b7fadad7
stevejk31 <stevekim31@gmail.com>
2016-03-15 15:08:39 -0700

    refactor barrel complete

--- js/barrel.js
+++ js/barrel.js
@@ -48,8 +48,8 @@ Barrel.prototype.move = function(pos, vel) {
   } else if (yPos > this.DIM_Y * 0.686 && yPos < this.DIM_Y * 0.7266 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level bottom
-  } else if (yPos > this.DIM_Y * 0.834 && yPos < this.DIM_Y * 0.874 ) {
-    vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000125];
+  } else if (yPos > this.DIM_Y * 0.832 && yPos < this.DIM_Y * 0.874 ) {
+    vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.00012];
     //fall right
   } else if (xPos >= this.DIM_X * 0.8033 && xPos <= this.DIM_Y * 0.8222 ){
     vel = [0,this.DIM_Y * 0.00222222222];

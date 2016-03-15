9928255a874ea4e3bad98b7cd8b289c4b1301d1c
stevejk31 <stevekim31@gmail.com>
2016-03-15 15:24:37 -0700

    barrel physics level 1

--- js/bundle.js
+++ js/bundle.js
@@ -373,7 +373,7 @@
 	  } else if (yPos > this.DIM_Y * 0.686 && yPos < this.DIM_Y * 0.7266 ) {
 	    vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
 	    // level bottom
-	  } else if (yPos > this.DIM_Y * 0.833 && yPos < this.DIM_Y * 0.87 ) {
+	  } else if (yPos > this.DIM_Y * 0.833 && yPos < this.DIM_Y * 0.868 ) {
 	    vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.00012];
 	    //fall right
 	  } else if (xPos >= this.DIM_X * 0.8033 && xPos <= this.DIM_Y * 0.8222 ){

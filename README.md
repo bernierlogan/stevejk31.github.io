23f2de559bb671cd828a4c571766e32669d5162b
stevejk31 <stevekim31@gmail.com>
2016-03-15 14:57:29 -0700

    refactor barrel not complete

--- README.md
+++ README.md
@@ -12,25 +12,25 @@ View it live at http://stevejk31.github.io/
 ```javascript
 Barrel.prototype.move = function(pos, vel) {
   //level 5 ramp
-  if (y_pos > this.DIM_Y * 0.236 && y_pos < this.DIM_Y * 0.278 ) {
+  if (yPos > this.DIM_Y * 0.236 && yPos < this.DIM_Y * 0.278 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 4
-  } else if (y_pos > this.DIM_Y * 0.3876 && y_pos < this.DIM_Y * 0.428 ) {
+  } else if (yPos > this.DIM_Y * 0.3876 && yPos < this.DIM_Y * 0.428 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 3
-  } else if (y_pos > this.DIM_Y * 0.537 && y_pos < this.DIM_Y * 0.578 ) {
+  } else if (yPos > this.DIM_Y * 0.537 && yPos < this.DIM_Y * 0.578 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 2
-  } else if (y_pos > this.DIM_Y * 0.686 && y_pos < this.DIM_Y * 0.7266 ) {
+  } else if (yPos > this.DIM_Y * 0.686 && yPos < this.DIM_Y * 0.7266 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level bottom
-  } else if (y_pos > this.DIM_Y * 0.829 && y_pos < this.DIM_Y * 0.8733 ) {
+  } else if (yPos > this.DIM_Y * 0.829 && yPos < this.DIM_Y * 0.8733 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     //fall right
-  } else if (x_pos >= this.DIM_X * 0.8033 && x_pos <= this.DIM_Y * 0.8222 ){
+  } else if (xPos >= this.DIM_X * 0.8033 && xPos <= this.DIM_Y * 0.8222 ){
     vel = [0,this.DIM_Y * 0.00222222222];
     //fall left
-  } else if (x_pos >= this.DIM_X * 0.1611 && x_pos <= this.DIM_Y * 0.1889 ){
+  } else if (xPos >= this.DIM_X * 0.1611 && xPos <= this.DIM_Y * 0.1889 ){
     vel = [0,this.DIM_Y * 0.00222222222];
   }
   this.pos =[ pos[0]+vel[0],  pos[1]+vel[1]];
--- js/barrel.js
+++ js/barrel.js
@@ -6,9 +6,9 @@ function Barrel(posArg) {
   this.pos = posArg["pos"];
   this.game = posArg["game"];
   this.color = "#FFA500";
-  this.radius = this.DIM_Y/90;
-  this.pos = [this.DIM_X/20, this.DIM_Y * 7/90];
-  this.vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
+  this.radius = 10;
+  this.pos = [45, 70];
+  this.vel = [2.025, 0.1323];
   this.speed = posArg["speed"];
   this.DIM_X = posArg["DIM_X"];
   this.DIM_Y = posArg["DIM_Y"];
@@ -32,30 +32,30 @@ Barrel.prototype.draw = function (ctx) {
 
 Barrel.prototype.move = function(pos, vel) {
   // level top
-  var y_pos = Math.floor(pos[1])
-  var x_pos = Math.floor(pos[0])
-  if (x_pos < this.DIM_X * 0.8056 && y_pos <= this.DIM_Y * 0.1278 ) {
+  var yPos = Math.floor(pos[1]);
+  var xPos = Math.floor(pos[0]);
+  if (xPos < this.DIM_X * 0.8056 && yPos <= this.DIM_Y * 0.1278 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 5
-  } else if (y_pos > this.DIM_Y * 0.236 && y_pos < this.DIM_Y * 0.278 ) {
+  } else if (yPos > this.DIM_Y * 0.236 && yPos < this.DIM_Y * 0.278 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 4
-  } else if (y_pos > this.DIM_Y * 0.3876 && y_pos < this.DIM_Y * 0.428 ) {
+  } else if (yPos > this.DIM_Y * 0.3876 && yPos < this.DIM_Y * 0.428 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 3
-  } else if (y_pos > this.DIM_Y * 0.537 && y_pos < this.DIM_Y * 0.578 ) {
+  } else if (yPos > this.DIM_Y * 0.537 && yPos < this.DIM_Y * 0.578 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level 2
-  } else if (y_pos > this.DIM_Y * 0.686 && y_pos < this.DIM_Y * 0.7266 ) {
+  } else if (yPos > this.DIM_Y * 0.686 && yPos < this.DIM_Y * 0.7266 ) {
     vel = [this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     // level bottom
-  } else if (y_pos > this.DIM_Y * 0.829 && y_pos < this.DIM_Y * 0.8733 ) {
+  } else if (yPos > this.DIM_Y * 0.829 && yPos < this.DIM_Y * 0.8733 ) {
     vel = [-this.DIM_X * 0.00225, this.DIM_Y * 0.000147];
     //fall right
-  } else if (x_pos >= this.DIM_X * 0.8033 && x_pos <= this.DIM_Y * 0.8222 ){
+  } else if (xPos >= this.DIM_X * 0.8033 && xPos <= this.DIM_Y * 0.8222 ){
     vel = [0,this.DIM_Y * 0.00222222222];
     //fall left
-  } else if (x_pos >= this.DIM_X * 0.1611 && x_pos <= this.DIM_Y * 0.1889 ){
+  } else if (xPos >= this.DIM_X * 0.1611 && xPos <= this.DIM_Y * 0.1889 ){
     vel = [0,this.DIM_Y * 0.00222222222];
   }
   this.pos =[ pos[0]+vel[0],  pos[1]+vel[1]];
@@ -65,7 +65,7 @@ Barrel.prototype.move = function(pos, vel) {
 Barrel.prototype.isCollidedWith = function(otherObject) {
   var xPos = this.pos[0] - otherObject.pos[0];
   var yPos = this.pos[1] - otherObject.pos[1];
-  var distance = Math.pow( (Math.pow(xPos, 2) + Math.pow(yPos, 2)), this.DIM_X * 5 /9000);
+  var distance = Math.pow( (Math.pow(xPos, 2) + Math.pow(yPos, 2)), 0.5);
   if ((distance < (this.radius + otherObject.radius)) && (this !== otherObject)) {
     this.collideWith(otherObject);
   }

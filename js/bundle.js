315544237fdbb33eb9bf6ac1047b21a02bfe6a5c
stevejk31 <stevekim31@gmail.com>
2016-03-15 14:13:23 -0700

    barrel level 5 physics bottom ramp

--- js/bundle.js
+++ js/bundle.js
@@ -348,7 +348,7 @@
 	  if (Math.floor(pos[0]) < this.DIM_X * 0.8056 && Math.floor(pos[1]) <= this.DIM_Y * 0.1278 ) {
 	    vel = [2.025, 0.1323];
 	    // level 5
-	  } else if (Math.floor(pos[1]) > this.DIM_Y * 0.236 && Math.floor(pos[1]) < this.DIM_Y * 0.28 ) {
+	  } else if (Math.floor(pos[1]) > this.DIM_Y * 0.236 && Math.floor(pos[1]) < this.DIM_Y * 0.278 ) {
 	    vel = [-2.025, 0.1323];
 	    // level 4
 	  } else if (Math.floor(pos[1]) > this.DIM_Y * 0.3876 && Math.floor(pos[1]) < this.DIM_Y * 0.4267 ) {

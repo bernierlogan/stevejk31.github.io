a890ab96a292174696e8ad6bf866d8d89a54ed2c
App Academy Student <student@appacademy.io>
2016-03-17 12:55:06 -0700

    node

--- js/popUp.jsx
+++ js/popUp.jsx
@@ -22,7 +22,6 @@ const customStyles = {
     bottom                : 'auto',
     marginRight           : '-50%',
     transform             : 'translate(-50%, -50%)',
-    text-align            : 'center'
   }
 };
 
